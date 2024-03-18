f9320e0b88817d0253c12db1e62a25381ee423f2
Khoa Tran <me@kcoder.dev>
2024-03-19 00:28:53 +0700

    fix: disable proxy format check

--- spotdl/download/downloader.py
+++ spotdl/download/downloader.py
@@ -215,8 +215,8 @@ class Downloader:
         proxy = self.settings["proxy"]
         proxies = None
         if proxy:
-            if not re.match(pattern=r"(http|https)://\d{1,5}", string=proxy):
-                raise DownloaderError(f"Invalid proxy server: {proxy}")
+            #if not re.match(pattern=r"(http|https)://\d{1,5}", string=proxy):
+            #    raise DownloaderError(f"Invalid proxy server: {proxy}")
             proxies = {"http": proxy, "https": proxy}
             logger.info("Setting proxy server: %s", proxy)
 
